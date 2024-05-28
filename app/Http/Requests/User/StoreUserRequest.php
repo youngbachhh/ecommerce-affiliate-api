0e253b52c6699935bc88d6339b2a65cc254da5b6
youngbachhh <91577458+youngbachhh@users.noreply.github.com>
2024-05-28 08:35:55 +0700

    Update StoreUserRequest.php

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -19,7 +19,7 @@ class StoreUserRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
-            'role_id' => 'required',
+            'role_id' => 'required|numeric',
         ];
     }
 
